32c2ae08c1741ab63bc9914ee9c8cf4b450544ab
Adam Moore <adam@mcdigital.jp>
2023-07-23 20:40:00 +0900

    back: update repositories for farm, fish pen

--- backend/types/errors.go
+++ backend/types/errors.go
@@ -1,3 +1,12 @@
 package types
 
+import "errors"
+
 const ServerErrorMessage = "Something's wrong. Please contact admin."
+const BadRequestErrorMessage = "Bad Request"
+
+var (
+	ErrNotFound  = errors.New("not found")
+	ErrDuplicate = errors.New("duplicate entry")
+	ErrInvalid   = errors.New("invalid")
+)
